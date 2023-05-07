f896a09eb4867d4e33c81dc39d4ee5caa111c5ea
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-05-07 16:13:57 +0800

    修改i18n的domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'zh-CN'],
+    defaultLocale: 'zh-CN',
+    domains: [
+      {
+        domain: 'qas.plumbiu.club',
+        defaultLocale: 'zh-CN'
+      },
+      {
+        domain: 'en_qas.plumbiu.club',
+        defaultLocale: 'en-US'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
