4ae4516bbbdeb40171baa3daa6ec61ed275b33d0
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-05-07 14:57:57 +0800

    增加i18n国际化

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   AppBar,
   Toolbar,
   Typography,
   Button,
+  Switch,
+  Stack,
 } from '@mui/material'
 import ToolbarMenu from './ToolbarMenu'
 import { useRouter } from 'next/router'
+import { useTranslation } from 'react-i18next'
 
 interface IRouterMap {
   [props: string]: string
 }
 
 export default function Header() {
+  const [switchStatus, setSwitchStatus] = useState(false)
+  const { t, i18n } = useTranslation()
   const routerMap: IRouterMap = {
     '/qa': 'U ask me to answer',
     '/sentence': 'Reading comprehension',
-    '/': ''
+    '/': '',
   }
   const router = useRouter()
   function backHome() {
     router.push('/')
   }
-
+  function switchHandler() {
+    setSwitchStatus(!switchStatus)
+    if (switchStatus) {
+      i18n.changeLanguage('en')
+    } else {
+      i18n.changeLanguage('zh')
+    }
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
           <ToolbarMenu />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {routerMap[router.pathname]}
-          </Typography>
-          <Button onClick={backHome} color="inherit">Home</Button>
+          {routerMap[router.pathname] === '' ? (
+            <Stack sx={{flexGrow: 1}} direction="row" spacing={1} alignItems="center">
+              <Typography>简体中文</Typography>
+              <Switch
+                color="default"
+                value={switchStatus}
+                onChange={switchHandler}
+                defaultChecked
+                inputProps={{ 'aria-label': 'ant design' }}
+              />
+              <Typography>en-US</Typography>
+            </Stack>
+          ) : (
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {routerMap[router.pathname]}
+            </Typography>
+          )}
+
+          <Button onClick={backHome} color="inherit">
+            {t('bar.home')}
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- components/Home.tsx
+++ components/Home.tsx
@@ -10,8 +10,10 @@ import React from 'react'
 import QAScore from './QAScore'
 import classes from '@/styles/home.module.css'
 import { useRouter } from 'next/router'
+import { useTranslation } from 'react-i18next'
 
 export default function Home() {
+  const { t } = useTranslation()
   const router = useRouter()
   function routerHandler(path: string) {
     router.push(path)
@@ -20,55 +22,55 @@ export default function Home() {
     <Card sx={{ minWidth: 275, marginTop: 8, paddingBottom: 8 }}>
       <CardContent sx={{ margin: 'auto', maxWidth: 650 }}>
         <Typography className={classes.center} sx={{ fontSize: 22, fontWeight: 700 }} gutterBottom>
-          You will see some card like this
+          {t('home.card')}
         </Typography>
         <Typography className={classes.demo} component="div">
-          <QAScore type="question" question="This is a question" />
-          <QAScore type="answer" answer="This is an answer" score={20} />
+          <QAScore type="question" question={t('home.question')} />
+          <QAScore type="answer" answer={t('home.answer') as string} score={20} />
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          What's this mean?
+          {t('home.mean')}
         </Typography>
         <Typography variant="body2" component="div">
-          <b>1.</b> The blue one is your question or passage
+          <b>1.</b> {t('home.blueCard')}
           <br />
           <div className={classes.questionDemo}>
-            <QAScore type="question" question="This is a question" />
+            <QAScore type="question" question={t('home.question')} />
           </div>
         </Typography>
         <Typography variant="body2" component="div">
-          <b>2.</b> The red one is the answer and score which indicates the
-          confident level
+          <b>2.</b> {t('home.redCard')}
           <br />
-          <QAScore type="answer" answer="This is an answer" score={20} />
+          <QAScore type="answer" answer={t('home.answer') as string} score={20} />
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          How to use it?
+          {t('home.use')}
         </Typography>
         <Typography variant="body2" gutterBottom>
-          put your question in the Input and Press the Button!
+          {t('home.input')}
           <br />
-          you will receive the answer from the server after a while
+          {t('home.receive')}
+          <br />
         </Typography>
         <Typography className={classes.center} color="text.secondary" gutterBottom component="div">
           <TextField size="small" label="question" variant="outlined" />
           <Button variant="contained">Press me</Button>
         </Typography>
         <Typography className={classes.center} sx={{ fontSize: 22, marginTop: 4, fontWeight: 700 }} gutterBottom>
-          About Me 
+          {t('home.about')}
         </Typography>
         <Typography className={classes.center} gutterBottom>
           <Button onClick={() => routerHandler('https://github.com/Plumbiu')} variant="contained" size="small">Github</Button>
-          <Button onClick={() => routerHandler('https://github.com/Plumbiu/nextjs_tensorflow')} variant="outlined" size="small">repo</Button>
+          <Button onClick={() => routerHandler('https://github.com/Plumbiu/nextjs_tensorflow')} variant="outlined" size="small">{t('home.repo')}</Button>
         </Typography>
         <Typography variant="body2" component="div">
         </Typography>
       </CardContent>
       <CardActions className={classes.center}>
         <Typography variant="body2">
-          This project is based on TensorFlow
+          {t('home.based')}
         </Typography>
-        <Button variant="outlined" onClick={() => routerHandler('https://github.com/tensorflow/tfjs-models')} size="small">Source Code</Button>
+        <Button variant="outlined" onClick={() => routerHandler('https://github.com/tensorflow/tfjs-models')} size="small">{t('home.source')}</Button>
       </CardActions>
     </Card>
   )
