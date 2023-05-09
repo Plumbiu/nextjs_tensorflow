6ae3419fd86a85e243469237f4d7d0921725a283
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-05-09 23:16:37 +0800

    网站图标正常显示

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         <title>TensorFlow WebUi</title>
       </Head>
       <body>
